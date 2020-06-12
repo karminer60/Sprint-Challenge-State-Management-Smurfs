6058fb269f2f416966aa2fe9b414585234980eea
karina-rodriguez <karminer60@gmail.com>
2020-06-12 14:13:44 -0700

    added post method to smurfForm

--- smurfs/src/App.js
+++ smurfs/src/App.js
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <div className="App">
       <SmurfList/>
+      <SmurfForm/>
     </div>
   );
 }
--- smurfs/src/components/smurfForm.js
+++ smurfs/src/components/smurfForm.js
@@ -1,77 +1,88 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import {useForm} from '../hooks/useForm.js';
+import { useForm } from "react-hook-form";
+import axios from 'axios';
+import { fetchSmurf } from '../store/actions/getAction.js';
 
 
-const SmurfForm = () => {
-    const [data, setData] = useState();
-    const { register, errors, handleSubmit } = useForm({
-      mode: "onBlur",
-    });
-    const onSubmit = (data) => {
-      setData(data);
-    };
+const SmurfForm = (props) => {
   
-    return (
-      <div className="App">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div>
-            <label htmlFor="name">Name*</label>
-            <input
-              id="name"
-              name="name"
-              placeholder="Smurf"
-              ref={register({ required: true, maxLength: 20 })}
-            />
-            {errors.firstName && (
-              <p>Looks like there was an error: {errors.name.type}</p>
-            )}
-          </div>
-  
-          <div>
-            <label htmlFor="age">Age*</label>
-            <input
-              id="age"
-              name="age"
-              placeholder="100"
-              ref={register({ required: true, maxLength: 20 })}
-            />
-            {errors.lastName && (
-              <p>Looks like there was an error: {errors.age.type}</p>
-            )}
-          </div>
+  const { register, errors, handleSubmit } = useForm({
+    mode: "onBlur",
+  });
+ 
+  const onSubmit = newSmurf => {
 
-          <div>
-            <label htmlFor="height">Age*</label>
-            <input
-              id="height"
-              name="height"
-              placeholder="5cm"
-              ref={register({ required: true, maxLength: 20 })}
-            />
-            {errors.lastName && (
-              <p>Looks like there was an error: {errors.height.type}</p>
-            )}
-          </div>
-  
-          
-          
-          
-          <input value="submit" type="submit" />
-        </form>
-      </div>
-    );
+    axios.post('http://localhost:3333/smurfs', newSmurf)
+      .then(res => {
+        props.fetchSmurf()
+
+      })
+      .catch(err => {
+        debugger
+      })
+  }
+
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="name">Name*</label>
+          <input
+            id="name"
+            name="name"
+            placeholder="Smurf"
+            ref={register({ required: true, maxLength: 20 })}
+          />
+          {errors.firstName && (
+            <p>Looks like there was an error: {errors.name.type}</p>
+          )}
+        </div>
+
+        <div>
+          <label htmlFor="age">Age*</label>
+          <input
+            id="age"
+            name="age"
+            placeholder="100"
+            ref={register({ required: true, maxLength: 20 })}
+          />
+          {errors.lastName && (
+            <p>Looks like there was an error: {errors.age.type}</p>
+          )}
+        </div>
+
+        <div>
+          <label htmlFor="height">Height*</label>
+          <input
+            id="height"
+            name="height"
+            placeholder="5cm"
+            ref={register({ required: true, maxLength: 20 })}
+          />
+          {errors.lastName && (
+            <p>Looks like there was an error: {errors.height.type}</p>
+          )}
+        </div>
+
+
+
+
+        <input value="submit" type="submit" />
+      </form>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+
   };
-  
-  const mapStateToProps = state => {
-    console.log(state);
-    return {
-      
-    };
-  };
-  
-  export default connect(
-    mapStateToProps,
-    {}
-  )(SmurfForm);
-  
+};
+
+export default connect(
+  mapStateToProps,
+  {fetchSmurf}
+)(SmurfForm);
