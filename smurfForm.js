9e5a4b5ae94afaeff0fff175e4ee6d3f065f66f3
karina-rodriguez <karminer60@gmail.com>
2020-06-12 10:29:21 -0700

    added input code to  smurfForm

--- smurfForm.js
+++ smurfForm.js
@@ -1,48 +1,66 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 
-// TODO:
-// - add redux flow for toggling the editing state
-// - add redux flow for updating the title
-const Title = props => {
-  const [newTitleText, setNewTitleText] = useState();
-  // const [state, dispatch] = useReducer(titleReducer, initialState);
 
-  const handleChanges = e => {
-    setNewTitleText(e.target.value);
+const ContactForm = () => {
+    const [data, setData] = useState();
+    const { register, errors, handleSubmit } = useForm({
+      mode: "onBlur",
+    });
+    const onSubmit = (data) => {
+      setData(data);
+    };
+  
+    return (
+      <div className="App">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label htmlFor="name">Name*</label>
+            <input
+              id="name"
+              name="name"
+              placeholder="Smurf"
+              ref={register({ required: true, maxLength: 20 })}
+            />
+            {errors.firstName && (
+              <p>Looks like there was an error: {errors.name.type}</p>
+            )}
+          </div>
+  
+          <div>
+            <label htmlFor="age">Age*</label>
+            <input
+              id="age"
+              name="age"
+              placeholder="100"
+              ref={register({ required: true, maxLength: 20 })}
+            />
+            {errors.lastName && (
+              <p>Looks like there was an error: {errors.age.type}</p>
+            )}
+          </div>
+
+          <div>
+            <label htmlFor="height">Age*</label>
+            <input
+              id="height"
+              name="height"
+              placeholder="5cm"
+              ref={register({ required: true, maxLength: 20 })}
+            />
+            {errors.lastName && (
+              <p>Looks like there was an error: {errors.height.type}</p>
+            )}
+          </div>
+  
+          
+          
+          
+          <input value="submit" type="submit" />
+        </form>
+      </div>
+    );
   };
-
-  return (
-    <div>
-      {!props.editing ? (
-        <h1>
-          {props.title} <i className="far fa-edit" />
-        </h1>
-      ) : (
-        <div>
-          <input
-            className="title-input"
-            type="text"
-            name="newTitleText"
-            value={newTitleText}
-            onChange={handleChanges}
-          />
-          <button>Update title</button>
-        </div>
-      )}
-    </div>
-  );
-};
-
-const mapStateToProps = state => {
-  console.log(state);
-  return {
-    title: state.titleReducer.title,
-    editing: state.titleReducer.editing
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  {}
-)(Title);
+  
+  export default ContactForm;
+  
